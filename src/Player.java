5c380be54563e8cea0cf2ff6a29f0f354ce3c4c4
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-07 18:10:43 -0800

    Player class comment position is now consistent with other similar classes

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,9 @@
 public class Player extends Creature
 {
-    //Player always has 100 health and @ as it's sprite
+
     Player(int x, int y)
     {
+        //Player always has 100 health and @ as it's sprite
         super(x, y, '@', 100);
     }
 }
