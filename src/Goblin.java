344b699147f4b9f37496421c1a314f547893186f
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-14 21:53:31 -0700

    Implemented Goblin Class

--- src/Goblin.java
+++ src/Goblin.java
@@ -1,13 +1,33 @@
-public class Goblin extends Creature
+public class Goblin extends Creature implements Hostile
 {
+    GameObject target;
+
     Goblin(int x, int y)
     {
         // Goblin always has 20 health and G as it's sprite
         super(x, y, 'G', 20);
     }
 
-    @Override
-    void update() {
+    public void setTarget(GameObject target)
+    {
+        this.target = target;
+    }
 
+    @Override
+    void update()
+    {
+        int distanceX = getX() - this.target.getX();
+        int distanceY = getY() - this.target.getY();
+
+        if(Math.abs(distanceX) > Math.abs(distanceY))
+            if(distanceX > 0)
+                this.move("a");
+            else
+                this.move("d");
+        else
+            if(distanceY > 0)
+                this.move("w");
+            else
+                this.move("s");
     }
 }
