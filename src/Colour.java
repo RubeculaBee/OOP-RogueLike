93bc225a240b5eb0420aa33a2e1cd6f5bb8beeff
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-14 23:56:06 -0700

    Removes colour enum

--- src/Colour.java
+++
@@ -1,6 +0,0 @@
-// Refers to colours that GameObject sprites can be drawn in
-public enum Colour
-{
-    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE,
-    BRIGHT_BLACK, BRIGHT_RED, BRIGHT_GREEN, BRIGHT_YELLOW, BRIGHT_BLUE, BRIGHT_MAGENTA, BRIGHT_CYAN, BRIGHT_WHITE
-}
