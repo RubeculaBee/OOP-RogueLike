d4255d973a250c5d8c874174494e60833383f109
Robin Lane <183433480+rubeculabee@users.noreply.github.com>
2025-03-15 18:00:33 -0700

    main adds 4 random creatures to room

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -9,7 +9,14 @@ public class GameLoop
     {
         World testRoom = new World(18,9, Layout.EMPTY);
         testRoom.terrainUpdate();
-        testRoom.addCreature(new Sheep(5,5));
+
+        // disgusting code
+        for(int i = 0; i < 4; i++)
+            testRoom.addCreature(switch((int)(Math.random()*2))
+            {
+                case 0 -> new Goblin( ((int)(Math.random()*(testRoom.width-2))+1), ((int)(Math.random()*(testRoom.height-2))+1), testRoom.player);
+                default -> new Sheep( ((int)(Math.random()*(testRoom.width-2))+1), ((int)(Math.random()*(testRoom.height-2))+1) );
+            });
 
         while(true)
         {
